6ab219a04131d737fbc4b8387e8dc1cdd807b13a
Chad Jemmett <chad.jemmett@gmail.com>
2019-05-01 15:47:21 -0600

    Trying to do fancy stuff with the photos

--- src/components/ItemPreview.js
+++ src/components/ItemPreview.js
@@ -4,14 +4,23 @@ import React from 'react';
 // const { project } = props.location.state.projData
 // console.log("project", project)
 
+          // <img src={props.projData.image}  alt={props.projData.name}/>
 const ItemPreview = (props) => {
+  const divStyle = {
+    backgroundImage: `url(${props.projData.image})`,
+    height: '150px',
+    maxWidth: '50%',
+    borderRadius: "50%",
+  }
+
+
   return (
       <div>
         <div className="preview-name">
           {props.projData.name}
         </div>
         <div className="preview-image">
-          <img src={props.projData.image}  alt={props.projData.name}/>
+          <div style={divStyle}></div>
         </div>
       </div>
       )
