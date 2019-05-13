098586b64e432877d940a6018fe1de22bd7ffc56
Chad Jemmett <chad.jemmett@gmail.com>
2019-05-13 10:12:51 -0600

    Fixed a flaw in my filter function in the Item Page.

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -4,7 +4,19 @@ import projectData from '../assets/json_data.js'
 
 
 const ItemPage = props  =>  {
-  const projData = projectData.projects.filter(item => item.id === props.match.params.id)[0];
+  // console.log("from params", props.match.params.id[0] )
+  //
+  const projData = projectData.projects.find(item => {
+    let paramId = Number(props.match.params.id)
+    return item.id === paramId 
+  })
+
+  console.log("project datat from find", projData)
+  // console.log("array: ", projectData.projects)
+
+  // const projData = projectData.projects.find(item => item.id === props.match.params.id);
+  // console.log("proj data: ", projData)
+  // console.log(projData);
         return(
             <div>
               <div>
