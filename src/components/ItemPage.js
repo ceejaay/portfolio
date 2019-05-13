3509fbc0ce4a98aad7b48b08db5e83b0f1e762dd
Chad Jemmett <chad.jemmett@gmail.com>
2019-05-13 14:01:28 -0600

    Working on the individual project page

--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -10,53 +10,40 @@ const ItemPage = props  =>  {
     let paramId = Number(props.match.params.id)
     return item.id === paramId 
   })
-
-  // console.log("array: ", projectData.projects)
-
-  // const projData = projectData.projects.find(item => item.id === props.match.params.id);
-  // console.log("proj data: ", projData)
-  // console.log(projData);
         return(
             <div className="project-page-container">
               <div className="project-page-name">
                 <h1>{projData.name}</h1>
               </div>
-              <div className="project-page-image" >
-                <img src={projData.image}  alt={projData.title}/>
-              </div>
-              <div className="project-page-description" >
-                <p>
-                  {projData.description}
-                </p>
+
+              <div className="project-imaage-description">
+                <div className="project-page-image">
+                  <img src={projData.image}  alt={projData.title}/>
+                </div>
+                <div className="project-page-description">
+                  <p>
+                    {projData.description}
+                  </p>
+                </div>
                 <div className="project-page-notes">
                   <b>
                     {projData.notes}
                   </b>
                 </div>
-
-
-                <div className="project-page-links">
-                  <a href={projData.links.liveSite}>
-                    <div>
-                      Live Project Link
-                    </div>
-                  </a>
-                  <a href={projData.links.github}>
-                    <div>
-                      Github Repository
-                    </div>
-                  </a>
-                  <a href={projData.links.pullRequests}>
-                    <div>
-                      Link to My Pull Requests
-                    </div>
-                  </a>
-                </div>
+              </div>
+              <div className="project-page-links">
+                <a href={projData.links.liveSite}>
+                  Live Project Link
+                </a>
+                <a href={projData.links.github}>
+                  Github Repository
+                </a>
+                <a href={projData.links.pullRequests}>
+                  Link to My Pull Requests
+                </a>
               </div>
             </div>
             )
-  // const { projData } = props.location.state;
-  // console.log("links?", projData.links.liveSite);
 }
 
 
